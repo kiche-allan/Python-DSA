e64e7adf22193dba3a1530aa45b57a1a9c5b1e26
allan Kiche <allankiche18@gmail.com>
2022-09-19 17:04:47 +0300

    first commit

---
+++ lists/lists.py
@@ -0,0 +1,10 @@
+# lists
+
+
+myList1 = [10, 30, 50, 70]
+myList2 = [20, 40, 60, 80]
+
+if myList1 in myList2:
+    print('Value is present in my list')
+else:
+    print('Value is not present in my list')
